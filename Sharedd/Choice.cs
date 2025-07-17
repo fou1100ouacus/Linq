bce0d92a38197caaaf39e38dcb3ad1ea6014ca11
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-07-17 15:20:58 +0300

    Add files via upload

---
+++ Sharedd/Choice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharedd
+{
+    public class Choice
+    {
+        public int Order { get; set; }
+        public string Description { get; set; }
+    }
+}
