7e41df4d0a5d7af20bef91b5cc70433eef38b5c2
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-07-17 15:20:04 +0300

    Add files via upload

---
+++ DefaultIfEmpty/Program.cs
@@ -0,0 +1,25 @@
+﻿using Sharedd;
+
+namespace DefaultIfEmpty
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var questions = Enumerable.Empty<Question>();
+
+            //var question2 = questions.DefaultIfEmpty();
+
+            //var question3 = questions.DefaultIfEmpty(Question.Default);
+
+            //question3.Toquize();
+
+            //Console.ReadKey();
+
+
+
+
+        }
+    }
+}
